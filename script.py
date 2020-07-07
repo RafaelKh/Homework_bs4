45c7228e493a14275e072ae20272d746afa269c3
RafaelKh <44085486+RafaelKh@users.noreply.github.com>
2020-07-08 00:49:21 +0400

    Add bs4 homework script
    Add bs4 homework script

---
+++ script.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+
+html = """<div>
+    <p>
+        1. my text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries, but also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently...
+    </p>
+    <p>
+        2. my text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries, but also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently...
+        </p>
+</div>
+"""
+s = BeautifulSoup(html, "html.parser")
+
+ps = s.find_all("p")
+list_with_texts = []
+list_with_texts.append(ps[0].contents[0])
+list_with_texts.append(ps[1].contents[0])
+print(list_with_texts)
