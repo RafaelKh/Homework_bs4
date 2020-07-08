be5d2f5887575fd041d0b719fa2d4a73f198c6b2
RafaelKh <44085486+RafaelKh@users.noreply.github.com>
2020-07-08 16:16:02 +0400

    Added signs deleting script
    Added signs deleting script

---
+++ delete_signs.py
@@ -0,0 +1,16 @@
+import unicodedata
+import sys
+
+text_data = [
+'Test!!!! hello. esim. test2...',
+'100% ola!! #HashTag',
+'testText???!!?'
+]
+#print(unicodedata.category("!"))
+#Nd, Ll, Lu, Zs
+new_list = ['', '', '']
+for i in range(len(text_data)):
+    for j in range(len(text_data[i])):
+        if unicodedata.category(text_data[i][j])[0] != 'P':
+            new_list[i] += text_data[i][j]
+print(new_list)
